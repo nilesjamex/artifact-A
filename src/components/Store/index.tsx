ab9b2da3dd866f201e7de896be945b61dfcb05a6
Niles <69752988+nilesjamex@users.noreply.github.com>
2026-03-03 07:17:17 +0000

    chore: storefront completed

---
+++ src/components/Store/index.tsx
@@ -0,0 +1,98 @@
+import { component$ } from "@builder.io/qwik";
+// import type { DocumentHead } from "@builder.io/qwik-city";
+import "./store.css";
+import Product from "../../assets/product.png?quality=100&jsx";
+
+export const Store = component$(() => {
+  const products = [
+    {
+      id: 1,
+      name: "Postman T-Shirt",
+      price: 15.99,
+      image: Product,
+    },
+    {
+      id: 2,
+      name: "Postman Mug",
+      price: 9.99,
+      image: Product,
+    },
+    {
+      id: 3,
+      name: "Postman Sticker Pack",
+      price: 4.99,
+      image: Product,
+    },
+    {
+      id: 1,
+      name: "Postman T-Shirt",
+      price: 15.99,
+      image: Product,
+    },
+    {
+      id: 2,
+      name: "Postman Mug",
+      price: 9.99,
+      image: Product,
+    },
+    {
+      id: 3,
+      name: "Postman Sticker Pack",
+      price: 4.99,
+      image: Product,
+    },
+    {
+      id: 1,
+      name: "Postman T-Shirt",
+      price: 15.99,
+      image: Product,
+    },
+    {
+      id: 2,
+      name: "Postman Mug",
+      price: 9.99,
+      image: Product,
+    },
+    {
+      id: 3,
+      name: "Postman Sticker Pack",
+      price: 4.99,
+      image: Product,
+    },
+  ];
+  return (
+    <div class="storefront">
+      <div class="storefront__header">
+        <h1 class="storefront__title">Welcome to your StoreFront</h1>
+        <h2 class="storefront__description">Powered by Filament.</h2>
+      </div>
+      <div class="storefront__product">
+        <h3>All Products</h3>
+        <div class="storefront__cards">
+          {products.map((product) => (
+            <div class="storefront__card" key={product.id}>
+              <div class="image__container">
+                <product.image class="product__image" alt="product image" />
+              </div>
+              <div class="storefront__card__text">
+                <h4 class="product__title">{product.name}</h4>
+                <h5 class="product__price">{product.price} USDC</h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+});
+
+// export const head: DocumentHead = {
+//   title: "StoreFront",
+//   meta: [
+//     {
+//       name: "description",
+//       content:
+//         "Welcome to the StoreFront, where you can find the best products at unbeatable prices.",
+//     },
+//   ],
+// };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,28 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Store } from "../components/Store";
 
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
+      {/* <h1>Hi 👋</h1>
       <div>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
-      </div>
+      </div> */}
+      <Store />
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Qwik Starter",
+  title: "StoreFront",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content:
+        "Welcome to the StoreFront, where you can find the best products at unbeatable prices.",
     },
   ],
 };
