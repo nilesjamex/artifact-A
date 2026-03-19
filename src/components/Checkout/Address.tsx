edfb1c0c9a09b1a05fae7dd622fd62035cf0cc5e
Niles <69752988+nilesjamex@users.noreply.github.com>
2026-03-19 10:03:57 +0000

    chore: code cleanup

--- src/components/Checkout/Address.tsx
+++ src/components/Checkout/Address.tsx
@@ -1,11 +1,8 @@
 import { component$, useContext } from "@builder.io/qwik";
 import "./address.css";
 import { CartContext } from "~/context/cart.context";
+import type { CartProduct } from "~/context/cart.context";
 import { Link } from "@builder.io/qwik-city";
-
-// export interface AddressProp {
-//   step: Signal<number>;
-// }
 
 export const CheckoutAddress = component$(() => {
   const cart = useContext(CartContext);
@@ -31,7 +28,7 @@ export const CheckoutAddress = component$(() => {
                 <input type="email" name="email" placeholder="Email" />
               </div>
               <div>
-                <label for="phone">Phoe Number</label>
+                <label for="phone">Phone Number</label>
                 <input type="tel" name="phone" placeholder="Phone Number" />
               </div>
             </div>
@@ -39,7 +36,7 @@ export const CheckoutAddress = component$(() => {
               <div>
                 <label for="address">Address Line</label>
                 <input
-                  type="address"
+                  type="text"
                   name="address"
                   placeholder="Street address, P.O. Box, company name, c/o"
                 />
@@ -71,46 +68,56 @@ export const CheckoutAddress = component$(() => {
             </div>
           </div>
         </div>
+
         <div class="address__order">
           <h4>
             Order Summary{" "}
             <span>&#40;{cart.items.value.totalProducts} items&#41;</span>
           </h4>
           <div class="address__summary">
-            {cart.items.value.products.map((item: any) => (
-              <div class="ccart__item" key={item.id}>
-                <div class="ccart__img">
-                  <img
-                    src={item.thumbnail}
-                    class="product__image"
-                    alt="product image"
-                    width={80}
-                    height={80}
-                  />
-                </div>
+            <div>
+              {cart.items.value.products.map((item: CartProduct) => (
+                <div class="ccart__item" key={item.id}>
+                  <div class="ccart__img">
+                    <img
+                      src={item.thumbnail}
+                      class="product__image"
+                      alt="product image"
+                      width={80}
+                      height={80}
+                    />
+                  </div>
 
-                <div class="ccart__item__content">
-                  <div class="ccart__item__name">
-                    <h4>{item.title}</h4>
-                  </div>
-                  <div class="ccart__item__price">
-                    <h5>1 X {item.price} USDC</h5>
+                  <div class="ccart__item__content">
+                    <div class="ccart__item__name">
+                      <h4>{item.title}</h4>
+                    </div>
+                    <div class="ccart__item__price">
+                      <h5>
+                        {item.quantity} × {item.price} USDC
+                      </h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
             <div class="ccart__prices">
               <div>
                 <h5>Subtotal</h5>
-                <h6>112.96 USDC</h6>
+                <h6>{cart.items.value.total} USDC</h6>
               </div>
               <div>
-                <h5>Taxes</h5>
-                <h6>112.96 USDC</h6>
+                <h5>Taxes (10%)</h5>
+                <h6>{cart.items.value.discountedTotal} USDC</h6>
               </div>
               <div>
                 <h5>Total</h5>
-                <h6>112.96 USDC</h6>
+                <h6>
+                  {(
+                    cart.items.value.total + cart.items.value.discountedTotal
+                  ).toFixed(2)}{" "}
+                  USDC
+                </h6>
               </div>
             </div>
             <Link href="/order/completed">
